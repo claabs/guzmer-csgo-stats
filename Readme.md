17ee6835e1b1324d068e4e3dddbb7da6fc18e8de
Charlie Laabs <charlielaabs@gmail.com>
2020-07-18 11:51:08 -0500

    Update to JSON import and add more stats

--- Readme.md
+++ Readme.md
@@ -2,12 +2,12 @@
 
 I could have done this with a proper data science language, but I'm more comfortable in Javascript.
 This just parses some stats from my demos that [CSGO Demos Manager](https://github.com/akiver/csgo-demos-manager) couldn't provide for me.
-As a bonus, this also has types for the General, Players, and Rounds sheets that export from CSGO Demos Manager.
+As a bonus, this also has types for the JSON that exports from CSGO Demos Manager.
 
 ## Setup
 
 1. In CSGO Demos Manager, select the matches you want to export.
-1. Click "Export as XLS", and export as multiple files.
+1. Right click, "Export JSON", and export as multiple files.
 1. Save files to a directory above the project directory ('../')
 1. `npm i`
 1. `npm run start:ts`
@@ -16,23 +16,31 @@ As a bonus, this also has types for the General, Players, and Rounds sheets that
 
 ```json
 {
-  "statsForMap": "de_mirage",
+  "statsForMap": [
+    "de_mirage",
+    "de_chlorine"
+  ],
+  "since": "2020-06-10T05:00:00.000Z",
   "minGuzmerPlayers": 4,
-  "clanMatches": 233,
-  "totalPistolRounds": 464,
-  "totalPistolRoundsWon": 203,
-  "totalTSideRounds": 2832,
-  "totalTSideRoundsWon": 1381,
-  "totalCTSideRounds": 3070,
-  "totalCTSideRoundsWon": 1731,
-  "pistolRoundWinRate": 0.4375,
-  "tSideWinRate": 0.4876412429378531,
-  "ctSideWinRate": 0.5638436482084691
+  "mapsParsed": 36,
+  "scrimmageMaps": 34,
+  "mapsWon": 13,
+  "totalPistolRounds": 71,
+  "totalPistolRoundsWon": 24,
+  "totalTSideRounds": 503,
+  "totalTSideRoundsWon": 178,
+  "totalCTSideRounds": 320,
+  "totalCTSideRoundsWon": 183,
+  "mapWinRate": 0.3611111111111111,
+  "pistolRoundWinRate": 0.3380281690140845,
+  "tSideWinRate": 0.3538767395626243,
+  "ctSideWinRate": 0.571875,
+  "tSideStart": 31,
+  "ctSideStart": 5,
+  "tSideStartRate": 0.8611111111111112
 }
 ```
 
 ## To Do
 
-As I was writing this readme, I realized you can export matches as JSON in the right click menu. Probably should just use that instead of converting XLSX to JSON (lol).
-
-Also, I should relocate the demos location to a subdirectory, instead of the parent.
+I should relocate the demos location to a subdirectory, instead of the parent.
--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 /* eslint-disable no-console */
 /* eslint-disable import/extensions */
-import * as xlsx from 'xlsx';
 import * as fs from 'fs';
 import * as path from 'path';
-import { SheetNames, PlayersRow, RoundsRow, RoundType, GeneralRow, Side } from './demo-xls';
+import { DemoJson, RoundType, Side, Round } from './json-types';
 
 const GUZMER_PLAYERS = [
   '76561198000253201', // Charlie L
@@ -15,69 +14,82 @@ const GUZMER_PLAYERS = [
   '76561198143832972', // Brendan
   '76561198041643113', // Charlie M
   '76561198155085307', // Katie
+  '76561198150706831', // Hutno
+  '76561198382666964', // Josh
+  '76561198347325486', // Scoop Life
+  '76561198025465711', // Phil
 ];
 
 const MIN_GUZMER_PLAYERS = 4;
 
-const MAP_FILTER = 'de_mirage';
+const MAP_FILTER = ['de_mirage'];
+
+// const DEMOS_AFTER = new Date('2020-06-10T00:00:00-05:00');
+const DEMOS_AFTER = new Date('2020-06-27T00:00:00-05:00');
 
 const outData = {
   statsForMap: MAP_FILTER,
+  since: DEMOS_AFTER,
   minGuzmerPlayers: MIN_GUZMER_PLAYERS,
-  clanMatches: 0,
+  mapsParsed: 0,
+  scrimmageMaps: 0,
+  mapsWon: 0,
   totalPistolRounds: 0,
   totalPistolRoundsWon: 0,
   totalTSideRounds: 0,
   totalTSideRoundsWon: 0,
   totalCTSideRounds: 0,
   totalCTSideRoundsWon: 0,
+  mapWinRate: 0,
   pistolRoundWinRate: 0,
   tSideWinRate: 0,
   ctSideWinRate: 0,
+  tSideStart: 0,
+  ctSideStart: 0,
+  tSideStartRate: 0,
 };
 
-const isTeamOnSide = (side: Side, teamName: string, round: RoundsRow): boolean => {
-  if (round['Winner Clan Name'] === teamName) {
-    return round.Winner === side;
+const isTeamOnSide = (side: Side, teamName: string, round: Round): boolean => {
+  if (round.winner_name === teamName) {
+    return round.winner_side === side;
   }
-  return round.Winner !== side;
+  return round.winner_side !== side;
+};
+
+const isScrimmage = (demo: DemoJson): boolean => {
+  return demo.id.includes('scrimmagemap');
 };
 
 const main = (): void => {
-  const dataFiles = fs.readdirSync('../').filter(file => file.endsWith('.xlsx'));
+  const dataFiles = fs.readdirSync('../').filter(file => file.endsWith('.json'));
 
   dataFiles.forEach(dataFile => {
-    const workbook = xlsx.readFile(path.join('..', dataFile));
-    const players = xlsx.utils.sheet_to_json<PlayersRow>(workbook.Sheets[SheetNames.Players]);
-    const rounds = xlsx.utils.sheet_to_json<RoundsRow>(workbook.Sheets[SheetNames.Rounds]);
-    const general = xlsx.utils.sheet_to_json<GeneralRow>(workbook.Sheets[SheetNames.General]);
+    const demo: DemoJson = JSON.parse(fs.readFileSync(path.join('..', dataFile), 'utf8'));
+    const { players, rounds } = demo;
 
-    const guzmerPlayers = players.filter(player => GUZMER_PLAYERS.includes(player.SteamID));
+    const guzmerPlayers = players.filter(player => GUZMER_PLAYERS.includes(player.steamid));
 
+    if (new Date(demo.date) < DEMOS_AFTER) return;
     // Must have enough Guzmer players
     if (guzmerPlayers.length < MIN_GUZMER_PLAYERS) return;
     // Filter on a map
-    if (general[0].Map !== MAP_FILTER) return;
+    if (!MAP_FILTER.includes(demo.map_name)) return;
 
     // Now we are a clan match
-    outData.clanMatches += 1;
+    outData.mapsParsed += 1;
+
+    if (isScrimmage(demo)) outData.scrimmageMaps += 1;
 
     // Find which team Guzmer is
-    const guzmerTeamName = guzmerPlayers[0].Team;
+    const guzmerTeamName = guzmerPlayers[0].team_name;
 
-    const pistolRounds = rounds.filter(round => round.Type === RoundType.PISTOL_ROUND);
+    const pistolRounds = rounds.filter(round => round.type === RoundType.PISTOL_ROUND);
     const tSideRounds = rounds.filter(round => isTeamOnSide(Side.T, guzmerTeamName, round));
     const ctSideRounds = rounds.filter(round => isTeamOnSide(Side.CT, guzmerTeamName, round));
 
-    const pistolRoundsWon = pistolRounds.filter(
-      round => round['Winner Clan Name'] === guzmerTeamName
-    );
-    const tSideRoundsWon = tSideRounds.filter(
-      round => round['Winner Clan Name'] === guzmerTeamName
-    );
-    const ctSideRoundsWon = ctSideRounds.filter(
-      round => round['Winner Clan Name'] === guzmerTeamName
-    );
+    const pistolRoundsWon = pistolRounds.filter(round => round.winner_name === guzmerTeamName);
+    const tSideRoundsWon = tSideRounds.filter(round => round.winner_name === guzmerTeamName);
+    const ctSideRoundsWon = ctSideRounds.filter(round => round.winner_name === guzmerTeamName);
 
     outData.totalPistolRounds += pistolRounds.length;
     outData.totalPistolRoundsWon += pistolRoundsWon.length;
@@ -85,10 +97,21 @@ const main = (): void => {
     outData.totalTSideRoundsWon += tSideRoundsWon.length;
     outData.totalCTSideRounds += ctSideRounds.length;
     outData.totalCTSideRoundsWon += ctSideRoundsWon.length;
+
+    if (demo.team_winner && demo.team_winner.team_name === guzmerTeamName) outData.mapsWon += 1;
+
+    if (demo.team_t.team_name === guzmerTeamName) {
+      outData.tSideStart += 1;
+    } else {
+      // console.log('Started CT on:', demo.date);
+      outData.ctSideStart += 1;
+    }
   });
+  outData.mapWinRate = outData.mapsWon / outData.mapsParsed;
   outData.pistolRoundWinRate = outData.totalPistolRoundsWon / outData.totalPistolRounds;
   outData.tSideWinRate = outData.totalTSideRoundsWon / outData.totalTSideRounds;
   outData.ctSideWinRate = outData.totalCTSideRoundsWon / outData.totalCTSideRounds;
+  outData.tSideStartRate = outData.tSideStart / outData.mapsParsed;
   console.log(JSON.stringify(outData, null, 2));
 };
 
